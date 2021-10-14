c96e7af2919171779e7d747c09cad6937daa8c43
APKUtils <apkutils@gmail.com>
2021-10-14 12:53:14 +0530

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,75 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+
+module.exports = {
+  title: "Piracy",
+  tagline: "It's illegal cuz they can't tax you!",
+  url: "https://piracyops.now.sh",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  themeConfig: {
+    image: "img/ogimage.png",
+    navbar: {
+      title: "Piracy",
+      logo: {
+        alt: "",
+        src: "img/logo.svg",
+        srcDark: "img/darklogo.svg",
+      },
+      items: [
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/APKUtils/piracy",
+          label: "GitLab",
+          position: "right",
+        },
+      ],
+    },
+    algolia: {
+      apiKey: "5470eae5ca52eb07867cedc3fdd33424",
+      indexName: "awesome_piracy",
+    },
+    prism: {
+      theme: require("prism-react-renderer/themes/dracula"),
+    },
+    googleAnalytics: {
+      trackingID: "UA-210244496-1",
+      anonymizeIP: true,
+    },
+    gtag: {
+      trackingID: "UA-210244496-1",
+      anonymizeIP: true,
+    },
+    announcementBar: {
+      id: "star",
+      content:
+        '<b><a target="_blank" href="https://github.com/APKUtils/piracy#:~:text=star">PLEASE LEAVE A STAR ON GITLAB</a></b>',
+      backgroundColor: "var(--ifm-background-surface-color)",
+      textColor: "var(--ifm-color-primary)",
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/APKUtils/piracy/-/edit/master/docs/",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: "all",
+            copyright: "It's free real estate! (CC0)",
+          },
+          editUrl: "https://github.com/APKUtils/piracy/-/edit/master/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
